e45064a9cbc76e18a843b103b33a3be3ab28b61b
stainsdas007 <stainsdas007@gmail.com>
2025-01-20 17:57:33 +0530

    Add files via upload

---
+++ Assignment4-4D.cs
@@ -0,0 +1,29 @@
+﻿namespace Assignment4_4D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //compare two string without using string library functions.
+
+
+            // Get two strings from the user
+            Console.Write("Enter the first string: ");
+            string str1 = Console.ReadLine();
+
+            Console.Write("Enter the second string: ");
+            string str2 = Console.ReadLine();
+
+                    //compare both string are same or not
+                    if (str1 == str2)
+                    {
+                        Console.WriteLine(" Both strings are same");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Both strings are not same");
+                    }              
+
+        }
+    }
+}
