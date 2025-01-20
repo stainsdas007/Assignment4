f2dc80abd91506268feecca9e727ca53c52a5979
stainsdas007 <stainsdas007@gmail.com>
2025-01-20 12:37:03 +0530

    Add files via upload

---
+++ Assignment4-1A.cs
@@ -0,0 +1,29 @@
+﻿//find the length of a string without using library function.
+namespace Assignment4_1A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            // Getting user input as the string
+            Console.Write("Enter a string: ");
+            string word = Console.ReadLine();
+
+            // Find the length manually
+            int length = 0;
+
+            // Loop for iterating each of the character 
+            foreach (char c in word)
+            {
+                length++;
+            }
+
+            // Display the length of the string
+            Console.WriteLine($"The length of the string is: {length}");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
